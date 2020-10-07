a87fe4c059ed0ed628228448f48be55065ef9d91
Pandiyan Murugan <PandiyanCool@users.noreply.github.com>
2020-10-07 13:35:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log('hello!')
+
+ (function (w,d,s,o,f,js,fjs) {
+            w['JS-Widget']=o;w[o] = w[o] || function () { (w[o].q = w[o].q || []).push(arguments) };
+            js = d.createElement(s), fjs = d.getElementsByTagName(s)[0];
+            js.id = o; js.src = f; js.async = 1; fjs.parentNode.insertBefore(js, fjs);
+        }(window, document, 'script', 'mw', 'https://cdn.document360.io/static/js/widget.js'));
+        mw('init', { apiKey: 'gsV1/lZ6yhHvUcvVv7zu0Pqlet0exqEaYGfFAWJuItgsWs+OkMrMzo93ZCkMZYHdi+rlk4w9gMZMVe0rLLcmnQWmH68wtWT3Ads8ce5sAH5OaTC2NdSXzJ1696ffobdkzp6S1fxxj4cjK6Y38m/fdQ==' });
+    
